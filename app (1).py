73806c045b8081e84dbeb4076e0ee0647b157610
muhammad ibrar Data scientist <81944365+123456170@users.noreply.github.com>
2025-09-03 23:35:35 +0500

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,22 @@
+import streamlit as st
+import random
+
+# Title
+st.title("✨ Random Quotes Generator")
+
+# List of quotes
+quotes = [
+    "The best way to get started is to quit talking and begin doing.",
+    "Don’t let yesterday take up too much of today.",
+    "It’s not whether you get knocked down, it’s whether you get up.",
+    "If you are working on something exciting, it will keep you motivated.",
+    "Success is not in what you have, but who you are.",
+    "Your limitation—it’s only your imagination.",
+    "Push yourself, because no one else is going to do it for you.",
+    "Great things never come from comfort zones."
+]
+
+# Button to generate a random quote
+if st.button("Generate Quote"):
+    quote = random.choice(quotes)
+    st.success(quote)
